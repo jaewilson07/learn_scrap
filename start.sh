2f72051cc44293a77fab9f4a3d6b16ce3e0b4e82
jaewilson07 <jaewilson07@gmail.com>
2025-12-19 07:01:03 +0000

    deployed to gcp

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:./src
-uvicorn legendary_potato.app.main:app --host 0.0.0.0 --port 8001 &
-sleep infinity
+uvicorn legendary_potato.app.main:app --host 0.0.0.0 --port ${PORT:-8001}
